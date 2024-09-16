97b3f06a3ac24314e68df662b134ffb8580483d2
Awais Waheed <awaiswaheed321@gmail.com>
2024-09-16 16:43:09 -0500

    Indentation Fixes

--- Lab7/src/main/java/com/test/prob1/partB/Employee.java
+++ Lab7/src/main/java/com/test/prob1/partB/Employee.java
@@ -3,44 +3,49 @@ package com.test.prob1.partB;
 import java.util.Objects;
 
 public class Employee {
-	private String name;
-	private int salary;
-	public Employee(String name, int salary) {
-		this.name = name;
-		this.salary = salary;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getSalary() {
-		return salary;
-	}
-	public void setSalary(int salary) {
-		this.salary = salary;
-	}
-	@Override 
-	public String toString() {
-		return "(" + name + ", " + salary + ")";
-	}
+    private String name;
+    private int salary;
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if (obj == null || getClass() != obj.getClass()) {
-			return false;
-		}
-		Employee employee = (Employee) obj;
-		return salary == employee.salary && name.equals(employee.name);
-	}
+    public Employee(String name, int salary) {
+        this.name = name;
+        this.salary = salary;
+    }
 
-	@Override
-	public int hashCode() {
-		return Objects.hash(name, salary);
-	}
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + name + ", " + salary + ")";
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null || getClass() != obj.getClass()) {
+            return false;
+        }
+        Employee employee = (Employee) obj;
+        return salary == employee.salary && name.equals(employee.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, salary);
+    }
 }
--- Lab7/src/main/java/com/test/prob1/partC/Employee.java
+++ Lab7/src/main/java/com/test/prob1/partC/Employee.java
@@ -1,40 +1,45 @@
 package com.test.prob1.partC;
 
 public class Employee {
-	private String name;
-	private int salary;
-	public Employee(String name, int salary) {
-		this.name = name;
-		this.salary = salary;
-	}
-	
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public int getSalary() {
-		return salary;
-	}
-	public void setSalary(int salary) {
-		this.salary = salary;
-	}
-	@Override 
-	public String toString() {
-		return "(" + name + ", " + salary + ")";
-	}
-	
-	@Override
-	public boolean equals(Object ob) {
-		if(ob == null) return false;
-		if(!(ob instanceof Employee)) return false;
-		Employee emp = (Employee)ob;
-		return emp.name.equals(name) && emp.salary == salary;
-	}
+    private String name;
+    private int salary;
 
-	@Override
-	public int hashCode() {
-		return name.hashCode() + Integer.hashCode(salary);
-	}
+    public Employee(String name, int salary) {
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "(" + name + ", " + salary + ")";
+    }
+
+    @Override
+    public boolean equals(Object ob) {
+        if (ob == null) return false;
+        if (!(ob instanceof Employee)) return false;
+        Employee emp = (Employee) ob;
+        return emp.name.equals(name) && emp.salary == salary;
+    }
+
+    @Override
+    public int hashCode() {
+        return name.hashCode() + Integer.hashCode(salary);
+    }
 }
