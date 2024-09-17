0eb4f2d2f569ff80c4e378de773aa275c89e57c8
Arslan Ametov <arsinux@yandex.com>
2024-09-17 14:35:04 -0500

    lab8

---
+++ Lab8/src/main/java/com/test/ProductInfo.java
@@ -0,0 +1,25 @@
+package com.test;
+
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+public class ProductInfo {
+	static enum SortMethod {
+		BYTITLE, BYPRICE
+	};
+
+	public static void sort(List<Product> emps, final SortMethod method) {
+		class ProductComparator implements Comparator<Product> {
+			@Override
+			public int compare(Product e1, Product e2) {
+				if (method == SortMethod.BYTITLE) {
+					return e1.title.compareTo(e2.title);
+				} else {
+					return Double.compare(e1.price, e1.price);
+				}
+			}
+		}
+		Collections.sort(emps, new ProductComparator());
+	}
+}
---
+++ Lab8/src/main/java/com/test/TitleComparator.java
@@ -0,0 +1,10 @@
+package com.test;
+
+import java.util.Comparator;
+
+public class TitleComparator implements Comparator<Product> {
+  @Override
+  public int compare(Product p1, Product p2) {
+    return p1.getTitle().compareTo(p2.getTitle());
+  }
+}
