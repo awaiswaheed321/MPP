dcad26a521a47e93af15c69a695544ad1e6787c9
Arslan Ametov <arsinux@yandex.com>
2024-09-23 12:22:20 -0500

    lab10 boilerplate

---
+++ Lab10/prob1/GroupUtil.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class GroupUtil {
+	
+	public static Group<?> copy(Group<?> group) {
+		return copyHelper(group);
+	}
+	
+	public static <T> Group<T> copyHelper(Group<T> group) {
+		List<T> elements = group.getElements();
+		Group<T> grp = new Group<T>(group.getSpecialElement(), elements);
+		return grp;
+	}
+	
+	public static void main(String[] args) {
+		List<Integer> list = Arrays.asList(0,1,2,3,4);
+		Group<Integer> group = new Group<>(0, list);
+		System.out.println(group);
+		System.out.println(GroupUtil.copy(group));
+	}
+}
---
+++ Lab10/prob4/Person.java
@@ -0,0 +1,9 @@
+public class Person {
+	String name;
+	public Person(String n) {
+		name = n;
+	}
+	public String getName() {
+		return name;
+	}
+}
